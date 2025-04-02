71c3d8c7b6eae67f49a46457f1b985c98071f727
TONNY7241 <anthonymwangi7241@gmail.com>
2025-04-02 15:11:11 +0300

    Add main.py with list operations

---
+++ main.py
@@ -0,0 +1,14 @@
+
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(100)
+my_list.append(40)
+my_list.insert(1, 15)
+my_list.extend([50, 60, 70])
+my_list.pop()
+my_list.sort()
+index_of_30 = my_list.index(30)
+print(f"The index of the value 30 in my list is: {index_of_30}")
+print(f"The final list is: {my_list}")
